079d5e40896025139aaee1544ad417bd3a463d3f
Cedric Strahl <cedric.strahl54@orange.fr>
2020-09-02 08:28:25 +0200

    travail_ete

---
+++ CodeRobot/Robot/Robot/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using WpfRobotInterface;
+
+namespace Robot
+{
+    class Program
+    {
+        static bool usingRobotInterface = true;
+        static RobotInterface interfaceRobot;
+        static void Main(string[] args)
+        {
+            if (usingRobotInterface)
+                StartRobotInterface();
+        }
+        static Thread t1;
+        static void StartRobotInterface()
+        {
+            t1 = new Thread(() =>
+            {
+                interfaceRobot = new RobotInterface();
+                interfaceRobot.ShowDialog();
+            });
+            t1.SetApartmentState(ApartmentState.STA);
+            t1.Start();
+        }
+    }
+}
